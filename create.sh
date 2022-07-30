8a9c41b88da34fe0b0f6f2b80a3430c0afdc7c90
David Baba <davidbabasalifu2020.com>
2022-07-30 14:32:56 +0100

    Commit to my project my final submit

--- create.sh
+++ create.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name %1 --template-body file://%2  --parameters file://%3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name serverInfro --template-body file://ServerInfra.yaml  --parameters file://Server-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
